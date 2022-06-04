f7c2791fd428c9b58c7553668d38c54f95b90ad2
pranta <prantaacharjee647@gmail.com>
2022-06-04 02:27:49 -0400

    added padding

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,20 @@
  import React from 'react';
-import { Form,Button} from 'react-bootstrap';
+import { Form,Button, Container} from 'react-bootstrap';
 import vector from "../../src/Vector.png";
 
 const Login = () => {
 
   return (
-        <div className='min-vh-100 d-flex justify-content-center align-items-center' style={{background:"whitesmoke"}}>        
+        <div className='min-vh-100 d-flex px-5 justify-content-center align-items-center' style={{background:"whitesmoke"}}>        
          <div className='mt-5 pt-5'>
         {/* logo div  */}
-          <div className='text-center' style={{background:' #0D6EFD',padding:'23px 30px',width:'85px',height:'70px',borderRadius: '37.5px 0px',marginLeft:'8rem'}}>
+          <div className='text-center' style={{background:' #0D6EFD',padding:'23px 30px',width:'85px',height:'70px',borderRadius: '37.5px 0px',marginLeft:'9rem'}}>
           <img className='img-fluid' src={vector} alt="logo" />
           </div>
          <h1 className='text-center py-2'>Please sign in</h1>
          {/* form section  */}
-            <Form className='pb-5'>
+         <Container>
+         <Form className='pb-5'>
             <Form.Group controlId="formBasicEmail">   
             <Form.Control style={{padding:'0.8rem',width:'366px'}} type="email" placeholder="Email Address" />
             </Form.Group>
@@ -27,6 +28,8 @@ const Login = () => {
               Sign Up
               </Button>
               </Form>
+         </Container>
+           
               <p className='text-center pt-5 mt-5'>&copy;2017-2021 Company Name</p>          
           </div>
          </div>
